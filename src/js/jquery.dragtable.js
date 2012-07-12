243e857a25f8d3c38effede139b6367f03527141
jebaird <jebaird@gmail.com>
2012-07-12 13:39:17 -0500

    code clean up, renamed vars

--- src/js/jquery.dragtable.js
+++ src/js/jquery.dragtable.js
@@ -127,45 +127,36 @@
 							
 				//position the drag dispaly to rel to the middle of the target co
 			
-				var self = this;
-				
-				//why is this getting stashed
-				var startingOffsetX = e.pageX - this.currentColumnCollectionOffset.x;
+				var self = this,
+					//used to position the dragdisplay against
+					startingColumnOffsetX = e.pageX - this.currentColumnCollectionOffset.x,
 				//TODO: make col switching relitvte to the silibing cols, not pageX
-                var prevMouseX = this.currentColumnCollectionOffset.x;
-
-					
+                //start from event cords
+                	prevMouseX = this.currentColumnCollectionOffset.x,
 					//get the colum count, used to contain col swap
-				var colCount = self.element[ 0 ]
+					colCount = self.element[ 0 ]
 									.getElementsByTagName( 'thead' )[ 0 ]
 									.getElementsByTagName( 'tr' )[ 0 ]
 									.getElementsByTagName( 'th' )
 									.length - 1;
 
-				
                 //drag the column around
 
                self._start( e )
                
-	            $( document )
-	                .bind('mousemove.' + self.widgetEventPrefix, function( e ){
-                    
-                	 var columnPos = self._setCurrentColumnCollectionOffset(),
-						colHalfWidth = Math.floor( self.currentColumnCollection[0].clientWidth / 2 );
-                   
-                    
-                    
-                    console.log( columnPos, startingOffsetX, e.pageX)
+	            $( document ).bind('mousemove.' + self.widgetEventPrefix, function( e ){
+	            	var columnPos = self._setCurrentColumnCollectionOffset(),
+	            		colHalfWidth = Math.floor( self.currentColumnCollection[0].clientWidth / 2 );
                                         
                     //console.log( 'half width colHalfWidth ', colHalfWidth)
                     self.dragDisplay
-                    	.css( 'left', ( e.pageX - startingOffsetX ) )
+                    	.css( 'left', ( e.pageX - startingColumnOffsetX ) )
                     
                     if( e.pageX < prevMouseX ){
                     	//move left
 							var threshold = columnPos.x;
 							
-							console.log( 'threshold ',threshold,  e.pageX - startingOffsetX )
+						//	console.log( 'threshold ',threshold,  e.pageX - startingColumnOffsetX )
 							if(e.pageX < threshold ){
 								self._swapCol(self.startIndex-1);
 							}
@@ -185,9 +176,7 @@
 			
                 })
                 .one( 'mouseup.' + self.widgetEventPrefix ,function(e ){
-
-                    self._stop( e )
-                    
+                    self._stop( e );
                 });
                           
 		},
