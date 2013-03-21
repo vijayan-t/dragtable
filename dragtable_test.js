7e4527c7f49d98681edda05bd7006782cc91b412
Jesse Baird <jebaird@gmail.com>
2013-03-21 09:40:24 -0500

    clean up of tests

--- dragtable_test.js
+++ dragtable_test.js
@@ -10,11 +10,11 @@ test("dragtable test", function(){
 	/*
 	 * drag col change
 	 */
-	S('#one th[data-header=first_name]').drag('+100 +0', function(){
+	S('#one th[data-header=first_name]').drag('th[data-header=last_name]', function(){
 		ok( S('#one th[data-header=last_name]').next().attr('data-header') == 'first_name', 'order changed')
 	})
 	//reverse
-	S('#one th[data-header=first_name]').drag('-150 +0', function(){
+	S('#one th[data-header=first_name]').drag('th[data-header=last_name]', function(){
 		ok( S('#one th[data-header=first_name]').next().attr('data-header') == 'last_name', ' reverse order changed')
 	})
 	
@@ -31,8 +31,8 @@ test("dragtable test", function(){
 	/*
 	 * handle
 	 */
-	S('#one th[data-header=phone_number] .dragtable-drag-handle').drag('-500 +0', function(){
-		ok( S('#one th:first').attr('data-header') == 'id', 'cant be dragged past boundary')
+	S('#one th[data-header=phone_number] .dragtable-drag-handle').drag('th[data-header=id]', function(){
+		ok( S('#one th:first').attr('data-header') == 'id', 'handle drag')
 		
 		ok( S('#one th:first').next().attr('data-header') == 'phone_number', 'tried to drag past boundary, is next to it')
 	})
@@ -50,7 +50,11 @@ test("dragtable test", function(){
 	/*
 	 * parent offset scroll
 	 */
-	
+	S('#one th[data-header=last_name]').drag('+2000 +0', function(){
+		//ok( S('#one th[data-header=salary]').prev().attr('data-header') == 'last_name', 'notdraggable cant be dragged')
+		
+		
+	})
 	
 });
 
